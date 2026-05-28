f776a9de5b7f259b2543ddecfbfbae9b31a33e72
Ubuntu <ubuntu@ip-172-26-8-22.ec2.internal>
2026-05-28 19:54:57 +0000

    ajusta gitignore e remove arquivos desnecessarios

--- teste.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config/config.php';
-
-unset($_SESSION['current_project_id']);
-unset($_SESSION['current_project_slug']);
-
-echo "Sessão limpa! <a href='/index.php'>Voltar ao escritório</a>";
